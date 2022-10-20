a4f0e0cb4a9276024a0d10e8ecb7b72821be9fd6
David Naranjo <dnaranjo0@misena.edu.co>
2022-10-20 00:36:03 -0500

    controls camera

--- 05_cameras/src/script.js
+++ 05_cameras/src/script.js
@@ -2,6 +2,20 @@ import './style.css'
 import * as THREE from 'three'
 
 /**
+ * Cursor Event
+ */
+const cursor = {
+    x: 0,
+    y: 0
+}
+window.addEventListener('mousemove', (event)=>{
+    //fix the number at -1 to 1 whit minus 0.5
+    // for fix the movement the axis invert catch all and multipy by minus (-)
+    cursor.x = - (event.clientX / sizes.width - 0.5) /* -0.5 to 0.5 */
+    cursor.y = event.clientY / sizes.height - 0.5 /* -0.5 to 0.5 */
+})
+
+/**
  * Base
  */
 // Canvas
@@ -36,13 +50,24 @@ scene.add(mesh)
 // whit the size parameters, can change the dimentions of canvas and modify the cube
 // Fix the cube dimensions whit the aspect ratio
 
-const aspectRatio = sizes.width / sizes.height
+// const aspectRatio = sizes.width / sizes.height
 // const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, .1, 100)
-const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, .1, 100)
+// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, .1, 100)
 
+// Controls whit the mouse ( events )
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, .1, 100)
 
-camera.position.x = 2
-camera.position.y = 2
+// CONTROLS
+// flyControl
+// FirtsPersonControl not for fps games
+// PointerLockControls - walk for example game
+// OrbitControl 
+// TrackBallControls whiout vertical limit
+// TransformControls 3d editor
+// DragControls, move objects
+
+// camera.position.x = 2
+// camera.position.y = 2
 camera.position.z = 2
 camera.lookAt(mesh.position)
 scene.add(camera)
@@ -54,15 +79,31 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 
 // Animate
-const clock = new THREE.Clock()
+// const clock = new THREE.Clock()
 
 const tick = () =>
 {
-    const elapsedTime = clock.getElapsedTime() / 2
+    // const elapsedTime = clock.getElapsedTime() / 2
 
     // Update objects
-    mesh.rotation.y = elapsedTime;
+    // mesh.rotation.y = elapsedTime;
 
+    // Update the camera
+    // between more number multipy more the rotation
+    // For see the back of cube Math.sin(cursor.x * Math.PI * 2) * 2
+    // camera.position.x = Math.sin(cursor.x * 10) * 3
+    // camera.position.z = Math.cos(cursor.x * 10) * 3
+    // camera.position.y = cursor.y * 3
+
+    camera.position.x = Math.sin(cursor.x * Math.PI * 10) * 3
+    camera.position.z = Math.cos(cursor.x * Math.PI * 10) * 3
+    camera.position.y = cursor.y * 3
+
+    // OPTION 1
+    camera.lookAt(mesh.position)
+    // OPTION 2
+    // camera.lookAt(new THREE.Vector3())
+    
     // Render
     renderer.render(scene, camera)
 
